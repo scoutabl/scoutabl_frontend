9d0655b14867af0504f67f3646201b82c57f06ee
areebbeigh <areebbeigh@gmail.com>
2025-07-09 03:18:13 +0530

    Adjust popup styles

--- src/api/onboarding/survey.js
+++ src/api/onboarding/survey.js
@@ -1,14 +1,30 @@
 import BaseAPI from "@/api/base";
 
 const ONBOARDING_CONFIG_URL = "/onboarding-config/"
+const SURVEY_URL = "/user-survey/"
 
 class SurveyAPI extends BaseAPI {
     constructor() {
         super();
     }
 
-    getOnboardingConfig() {
-        return this.get(ONBOARDING_CONFIG_URL).then(res => res.data.results?.[0] || null);
+    async getOrCreateOnboardingConfig() {
+        const config = (await this.get(ONBOARDING_CONFIG_URL)).data.results?.[0] || null;
+        if (!config) {
+            const res = await this.post(ONBOARDING_CONFIG_URL, {
+                extra: {
+                    assessmentOnboarding: {
+                        page: "test"
+                    }
+                }
+            });
+            return res;
+        }
+        return config;
+    }
+
+    async updateOnboardingConfig(config) {
+        return this.patch(ONBOARDING_CONFIG_URL, config);
     }
 }
 
--- src/components/HomePageModal.jsx
+++ src/components/HomePageModal.jsx
@@ -64,23 +64,25 @@ const LAUNCH = "launch";
 
 const HomePageModal = ({ onClose }) => {
   const { resolveEnum, enumsLoading } = useEnums();
-  const { config, setConfig } = useState(null);
-  useEffect(() => {
-    surveyAPI.getOnboardingConfig().then((data) => setConfig(data));
-  }, []);
-
-  const { page, setPage } = useState();
+  const [ config, setConfig ] = useState(null);
+  const [ page, setPage ] = useState(null);
   const [selectedOption, setSelectedOption] = useState(null);
   const [currentStep, setCurrentStep] = useState("test");
   const [direction, setDirection] = useState(0);
   const modalRef = useRef(null);
 
+  useEffect(() => {
+    surveyAPI.getOrCreateOnboardingConfig().then((config) => {
+      setConfig(config);
+      setPage(config.extra.assessmentOnboarding.page);
+    });
+  }, []);
+
   const SURVEY_PAGES = useMemo(() => {
     const pages = {
       test: {
         title: "Which kind of test fits your #hiring flow?",
-        subtitle:
-          "These answers help Scoutabl shape an experience that fits you like a tailored suit—minus the stitching",
+        subtitle: "These answers help Scoutabl shape an experience that fits you like a tailored suit",
         hint: "minus the stitching",
         options: [
           {
@@ -176,48 +178,6 @@ const HomePageModal = ({ onClose }) => {
     onClose();
   };
 
-  const options = [
-    {
-      id: "coding",
-      title: "Coding Challenges",
-      description: "Test technical chops and problem-solving",
-      icon: codingIcon,
-    },
-    {
-      id: "soft",
-      title: "Soft Skills & Personality",
-      description: "Gauge communication, empathy, and traits",
-      icon: bulbIcon,
-    },
-    {
-      id: "business",
-      title: "Business & Case Scenarios",
-      description: "Assess strategic thinking and decision-making",
-      icon: breifcaseIcon,
-    },
-  ];
-
-  const roles = [
-    {
-      id: "founder",
-      title: "Founder",
-      description: "Building the dream team from scratch",
-      icon: rocketIcon,
-    },
-    {
-      id: "hiring_manager",
-      title: "Hiring Manager",
-      description: "Making the final calls on the talent",
-      icon: arrowIcon,
-    },
-    {
-      id: "recruiter",
-      title: "Recruiter",
-      description: "Streamlining recruitment and hiring",
-      icon: userIcon,
-    },
-  ];
-
   const containerVariants = {
     hidden: { opacity: 0 },
     show: {
@@ -235,6 +195,12 @@ const HomePageModal = ({ onClose }) => {
     show: { opacity: 1, transition: { duration: 0.5 }, y: 0 },
   };
 
+  const renderPage = page != null ? SURVEY_PAGES[page] : null;
+  let title1, title2;
+  if (renderPage) {
+    [title1, title2] = renderPage.title.split("#");
+  }
+
   return (
     <motion.div
       variants={modalVariants}
@@ -246,7 +212,7 @@ const HomePageModal = ({ onClose }) => {
       style={{ willChange: "transform" }}
     >
       <AnimatePresence mode="wait" custom={direction}>
-        {currentStep === "test" ? (
+        {renderPage ? (
           <motion.div
             key="test-content"
             custom={direction}
@@ -265,7 +231,7 @@ const HomePageModal = ({ onClose }) => {
               // initial={{ opacity: 0, y: 20 }}
               // animate={{ opacity: 1, y: 0 }}
               // transition={{ duration: 0.4, delay: 0.2 }}
-              className="flex flex-col items-center justify-center"
+              className="flex flex-col justify-center"
             >
               <motion.h2
                 className="text-[32px] font-light pb-3"
@@ -273,19 +239,21 @@ const HomePageModal = ({ onClose }) => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.4, delay: 0.2 }}
               >
-                Which kind of test fits your{" "}
-                <span className="bg-gradient-to-r from-[#806BFF] to-[#A669FD] inline-block text-transparent bg-clip-text">
-                  #hiring flow?
+                {title1}
+                <span className="bg-gradient-to-r from-[#806BFF] to-[#A669FD] inline-block text-transparent bg-clip-text font-medium">
+                  #{title2}
                 </span>
               </motion.h2>
               <motion.p
-                className="text-[#5C5C5C] text-sm font-medium text-center"
+                className="text-[#5C5C5C] font-normal"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.4, delay: 0.4 }}
               >
-                These answers help Scoutabl shape an experience that fits you
-                like a tailored suit—minus the stitching
+                {renderPage.subtitle}<br />
+                <motion.p className="italic">
+                — {renderPage.hint}
+                </motion.p>
               </motion.p>
             </motion.div>
             <motion.div
@@ -294,13 +262,13 @@ const HomePageModal = ({ onClose }) => {
               initial="hidden"
               animate="show"
             >
-              {options.map((option) => (
+              {renderPage.options.map((option) => (
                 <motion.div
                   key={option.id}
                   variants={itemVariants}
                   onClick={() => setSelectedOption(option.id)}
                   className={cn(
-                    "h-full w-full px-[22px] py-[41px] flex flex-col items-center justify-center gap-2 [box-shadow:0px_16px_24px_rgba(0,_0,_0,_0.06),_0px_2px_6px_rgba(0,_0,_0,_0.04)] border rounded-[16px] bg-white hover:bg-[#FAEEFF] transition-all duration-300 ease-in-out group ",
+                    "h-full w-65 px-[22px] py-[65px] flex flex-col items-center justify-center gap-3 [box-shadow:0px_16px_24px_rgba(0,_0,_0,_0.06),_0px_2px_6px_rgba(0,_0,_0,_0.04)] border rounded-[16px] bg-white hover:bg-[#FAEEFF] transition-all duration-300 ease-in-out group text-center cursor-pointer z-10",
                     selectedOption === option.id
                       ? "bg-[#FAEEFF] border-[#9B71F7]"
                       : "bg-white border-black/10 hover:border-[#9B71F7]"
@@ -309,16 +277,16 @@ const HomePageModal = ({ onClose }) => {
                   <img
                     src={option.icon}
                     alt={`${option.title} Icon`}
-                    className={`w-10 h-10 pb-2 transition-colors duration-300 ${
+                    className={`w-12 h-12 mb-3 transition-colors duration-300 ${
                       selectedOption === option.id
                         ? "[filter:invert(41%)_sepia(56%)_saturate(1009%)_hue-rotate(226deg)_brightness(94%)_contrast(94%)]"
                         : "group-hover:[filter:invert(41%)_sepia(56%)_saturate(1009%)_hue-rotate(226deg)_brightness(94%)_contrast(94%)]"
                     }`}
                   />
-                  <h3 className="text-xs font-semibold text-[#333333]">
+                  <h4 className="font-semibold text-[#333333]">
                     {option.title}
-                  </h3>
-                  <span className="font-normal text-[10px] text-[#5C5C5C] text-center">
+                  </h4>
+                  <span className="font-normal text-[13px] text-[#5C5C5C] text-center">
                     {option.description}
                   </span>
                 </motion.div>
@@ -341,105 +309,7 @@ const HomePageModal = ({ onClose }) => {
               </Button>
             </motion.div>
           </motion.div>
-        ) : (
-          <motion.div
-            key="role-content"
-            custom={direction}
-            variants={stepVariants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            transition={{
-              type: "spring",
-              stiffness: 300,
-              damping: 30,
-            }}
-            className="flex flex-col items-center justify-center gap-[52px]"
-          >
-            <motion.div className="flex flex-col items-center justify-center">
-              <motion.h2
-                className="text-[32px] font-light pb-3"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.4, delay: 0.2 }}
-              >
-                Which best describes your{" "}
-                <span className="bg-gradient-to-r from-[#806BFF] to-[#A669FD] inline-block text-transparent bg-clip-text">
-                  #role
-                </span>
-                ?
-              </motion.h2>
-              <motion.p
-                className="text-[#5C5C5C] text-sm font-medium text-center"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.4, delay: 0.4 }}
-              >
-                Your role helps Scoutabl customize the experience to suit your
-                needs
-                <br />
-                —no fine-tuning necessary.
-              </motion.p>
-            </motion.div>
-            <motion.div
-              className="flex items-center justify-between gap-10 w-full"
-              variants={containerVariants}
-              initial="hidden"
-              animate="show"
-            >
-              {roles.map((option) => (
-                <motion.div
-                  key={option.id}
-                  variants={itemVariants}
-                  onClick={() => setSelectedOption(option.id)}
-                  className={cn(
-                    "flex-1 min-w-[213px] px-[22px] py-[41px] flex flex-col items-center justify-center gap-2 [box-shadow:0px_16px_24px_rgba(0,_0,_0,_0.06),_0px_2px_6px_rgba(0,_0,_0,_0.04)] border rounded-[16px] bg-white hover:bg-[#FAEEFF] transition-all duration-300 ease-in-out group ",
-                    selectedOption === option.id
-                      ? "bg-[#FAEEFF] border-[#9B71F7]"
-                      : "bg-white border-black/10 hover:border-[#9B71F7]"
-                  )}
-                >
-                  <img
-                    src={option.icon}
-                    alt={`${option.title} Icon`}
-                    className={`w-10 h-10 pb-2 transition-colors duration-300 ${
-                      selectedOption === option.id
-                        ? "[filter:invert(41%)_sepia(56%)_saturate(1009%)_hue-rotate(226deg)_brightness(94%)_contrast(94%)]"
-                        : "group-hover:[filter:invert(41%)_sepia(56%)_saturate(1009%)_hue-rotate(226deg)_brightness(94%)_contrast(94%)]"
-                    }`}
-                  />
-                  <h3 className="text-xs font-semibold text-[#333333]">
-                    {option.title}
-                  </h3>
-                  <span className="font-normal text-[10px] text-[#5C5C5C] text-center text-nowrap">
-                    {option.description}
-                  </span>
-                </motion.div>
-              ))}
-            </motion.div>
-            <motion.div
-              className="flex items-center gap-4"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.4, delay: 2.5 }}
-            >
-              <Button
-                onClick={handleBack}
-                className="bg-white hover:bg-gray-50 text-[#333333] rounded-full px-3 py-[6px] h-[33px] border border-gray-200"
-              >
-                <ChevronLeft className="h-4 w-4" />
-                Back
-              </Button>
-              <Button
-                onClick={() => handleRoleSelect("default")}
-                className="bg-[#8B5CF6] hover:bg-[#8B5CF6]/90 text-white rounded-full px-3 py-[6px] h-[33px] w-[83px]"
-              >
-                Next
-                <ChevronRight className="h-4 w-4" />
-              </Button>
-            </motion.div>
-          </motion.div>
-        )}
+        ) : (<p>TODO: Display Loading Icon</p>)}
       </AnimatePresence>
 
       <motion.img
